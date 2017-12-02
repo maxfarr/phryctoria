08935c58af64457174404ce7c2bde1c154a19366
Max Farr <maxandersfarr@gmail.com>
2017-12-02 08:15:21 -0800

    implemented shader loading, defining basic polygon classes

--- source/phryctoria/sandbox.cpp
+++ source/phryctoria/sandbox.cpp
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 
 //first GLEW
-#include <GL/glew.h>
+#include <GL\glew.h>
 //then GLFW
 #include <GLFW\glfw3.h>
 //and then GLM
 #include <glm\glm.hpp>
 using namespace glm;
+
+#include "LoadShader.cpp"
+
+GLFWwindow* window;
 
 int main()
 {
@@ -26,19 +30,17 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //potential osx port?
 
-	GLFWwindow* window;
 	window = glfwCreateWindow(1024, 768, "OpenGL Sandbox", NULL, NULL);
-
 	if (window == NULL)
 	{
 		fprintf(stderr, "GLFW failed to open a window\n");
 		glfwTerminate();
 		return -1;
 	}
-
 	glfwMakeContextCurrent(window);
-	glewExperimental = true;
 
+	//GLEW init
+	glewExperimental = true;
 	if (glewInit() != GLEW_OK)
 	{
 		fprintf(stderr, "GLEW failed to initialize\n");
@@ -47,8 +49,45 @@ int main()
 
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	GLuint programID = LoadShader("simple.vert", "simple.frag");
+
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
 	do
 	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(programID);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+		glVertexAttribPointer(
+			0,
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
+		);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDisableVertexAttribArray(0);
 
 		//swap buffers, poll for new events
 		glfwSwapBuffers(window);
